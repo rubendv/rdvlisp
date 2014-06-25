7aabc35de2e65c815f5e1a0065a2ad9d473043ef
Ruben De Visscher <ruben.de.visscher@gmail.com>
2014-06-25 11:09:17 +0200

    Initial Commit

---
+++ rdvlisp/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  rdvlisp
+//
+//  Created by Ruben De Visscher on 25/06/14.
+//  Copyright (c) 2014 Ruben De Visscher. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
